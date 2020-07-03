6b2bb0482b105f1b396696bb6c38edb74f7c0ea9
Ted M. Young <tedyoung@gmail.com>
2020-07-02 20:24:52 -0700

    Added purge options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  purge: [],
   theme: {
     extend: {},
   },
@@ -7,4 +6,18 @@ module.exports = {
   plugins: [
     require('@tailwindcss/ui'),
   ],
+  purge: {
+    options: {
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/
+      ]
+    },
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './public/**/*.html',
+      './src/**/*.vue'
+    ]
+  }
 }
